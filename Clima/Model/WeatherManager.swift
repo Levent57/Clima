39a65c40998dd5c0fc990190fe49da0621783c8b
Levent <levent.bostanci57@gmail.com>
2021-03-07 23:40:51 +0100

    first commit

---
+++ Clima/Model/WeatherManager.swift
@@ -0,0 +1,57 @@
+//
+//  WeatherManager.swift
+//  Clima
+//
+//  Created by Levent Bostanci on 04/03/2021.
+//  Copyright © 2021 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherManager {
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=ff3a9ff4566b31937e5fba57b930c53c&units=metric"
+    
+    func fetchWeather(cityName: String) {
+        let URLString = "\(weatherURL)&q=\(cityName)"
+        performRequest(urlString: URLString)
+    }
+    
+    func performRequest(urlString: String) {
+        
+        if let url = URL(string: urlString) {
+            
+            let session = URLSession(configuration: .default)
+            
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                
+                if let safeData = data {
+                    parseJSON(weatherData: safeData)
+                }
+            }
+            task.resume()
+        }
+    }
+    
+    func parseJSON(weatherData: Data) {
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
+            let id = decodedData.weather[0].id
+            let temp = decodedData.main.temp
+            let name = decodedData.name
+            
+            let weather = WeatherModel(conditionId: id, cityName: name, temperature: temp)
+            
+            print(weather.temperatureStinrg)
+        } catch {
+            print(error)
+        }
+    }
+    
+}
+
+
